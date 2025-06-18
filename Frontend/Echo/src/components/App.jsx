a8810bfb0613a796978f67b43df200c56eae02d9
Srijit Dangol <srijit.dngl@gmail.com>
2025-06-18 11:20:17 +0545

    landing page layout and regesiter page

--- Frontend/Echo/src/components/App.jsx
+++ Frontend/Echo/src/components/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './style.css';
 import Login from './Login'
+import Register from './Register';
+import MainApp from './MainApp';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <Login />
-      </div>
-  )
+      <Routes>
+        <Route path="/" element={<MainApp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+  );
 }
 
 export default App
